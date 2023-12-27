1d056e44fb863f75994ca70f8c872a2a6b1d61dd
CodiHaus <contact@codihaus.com>
2023-12-27 14:09:08 +0700

    chore(script): import ExtensionManifest

--- scripts/link.ts
+++ scripts/link.ts
@@ -1,4 +1,4 @@
-import { ExtensionManifest } from '@directus/constants';
+import { ExtensionManifest } from '@directus/extensions';
 import fs from 'fs-extra';
 import path from 'path';
 import { log } from '../node_modules/@directus/extensions-sdk/dist/cli/utils/logger.js';
